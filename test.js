1388e4546ff7fa453dffc3b1d441dd2748cdfea4
Caldwell <Chris.Caldwell@prometric.com>
2018-05-07 12:42:32 -0400

    Added feature to split/route messages to discrete state-based outputs and supporting tests

--- test.js
+++ test.js
@@ -24,6 +24,7 @@ var flow1 = [
         triggerPropertyType: "msg",
         stateProperty: "state",
         statePropertyType: "msg",
+        discreteOutputs: false,
         outputStateChangeOnly: false,
         throwException: false,
         states: ["start", "state1", "state2", "state3"],
@@ -99,6 +100,7 @@ var flow2 = [
         triggerPropertyType: "msg",
         stateProperty: "state",
         statePropertyType: "msg",
+        discreteOutputs: false,
         outputStateChangeOnly: true,
         throwException: false,
         states: ["start","state1","state2","state3"],
@@ -137,6 +139,7 @@ var flow3 = [
         triggerPropertyType: "msg",
         stateProperty: "state",
         statePropertyType: "msg",
+        discreteOutputs: false,
         outputStateChangeOnly: false,
         throwException: true,
         states: ["start","state1","state2","state3"],
@@ -156,10 +159,63 @@ var flow3 = [
     }
 ];
 
+var flow4 = [
+    { id:"f1", type:"tab", label:"Flow 1"},
+    {
+        id: "c1",
+        type: "catch",
+        name: "",
+        z: "f1",
+        scope: ["n1"],
+        wires: [["h1"]]
+    },
+    {
+        id: "n1",
+        type: "state-machine",
+        name: "Test",
+        z: "f1",
+        triggerProperty: "topic",
+        triggerPropertyType: "msg",
+        stateProperty: "state",
+        statePropertyType: "msg",
+        discreteOutputs: true,
+        outputStateChangeOnly: false,
+        throwException: false,
+        states: ["start","state1","state2","state3"],
+        transitions: [
+            {name: "t1", from: "start", to: "state1"},
+            {name: "t2", from: "state1", to: "state2"},
+            {name: "t3", from: "state2", to: "state3"},
+            {name: "t4", from: "state3", to: "state1"},
+            {name: "r", from: "*", to: "start"}
+        ],
+        wires: [["h1"],["h2"],["h3"],["h4"],]
+    },
+    {
+        id: "h1",
+        z: "f1",
+        type: "helper"
+    },
+    {
+        id: "h2",
+        z: "f1",
+        type: "helper"
+    },
+    {
+        id: "h3",
+        z: "f1",
+        type: "helper"
+    },
+    {
+        id: "h4",
+        z: "f1",
+        type: "helper"
+    }
+];
 
 describe('state-machine Node', function () {
 
-  afterEach(function () {
+  afterEach(function () { 
     helper.unload();
   });
 
@@ -171,6 +227,7 @@ describe('state-machine Node', function () {
       n1.should.have.property('triggerPropertyType', 'msg');
       n1.should.have.property('stateProperty', 'state');
       n1.should.have.property('statePropertyType', 'msg');
+      n1.should.have.property('discreteOutputs', false);
       n1.should.have.property('outputStateChangeOnly', false);
       n1.should.have.property('throwException', false);
       n1.fsm._fsm.config.should.have.property('states').with.length(5);
@@ -179,7 +236,7 @@ describe('state-machine Node', function () {
     });
   });
 
-    it('backwards compatible', function (done) {
+  it('backwards compatible', function (done) {
     helper.load([stateMachineNode, catchNode], flow1a, function () {
       var n1 = helper.getNode("n1");
       n1.should.have.property('name', 'Test');
@@ -187,6 +244,7 @@ describe('state-machine Node', function () {
       n1.should.have.property('triggerPropertyType', 'msg');
       n1.should.have.property('stateProperty', 'state');
       n1.should.have.property('statePropertyType', 'msg');
+      n1.should.have.property('discreteOutputs', false);
       n1.should.have.property('outputStateChangeOnly', false);
       n1.should.have.property('throwException', false);
       n1.fsm._fsm.config.should.have.property('states').with.length(5);
@@ -387,8 +445,6 @@ describe('state-machine Node', function () {
     });
   });
 
-
-
   it('invalid transition', function (done) {
     helper.load([stateMachineNode, catchNode], flow3, function () {
       var n1 = helper.getNode("n1");
@@ -426,5 +482,236 @@ describe('state-machine Node', function () {
     });
   });
 
+  it('discrete state outputs', function (done) {
+    helper.load([stateMachineNode, catchNode], flow4, function () {
+      var n1 = helper.getNode("n1");
+      var h1 = helper.getNode("h1");
+      var h2 = helper.getNode("h2");
+      var h3 = helper.getNode("h3");
+      var h4 = helper.getNode("h4");
+
+      n1.outputStateChangeOnly = true;
+
+      var state;
+      var outputCounters = {"start":0,"state1":0,"state2":0,"state3":0};
+      var expectedCounts = {"start":0,"state1":0,"state2":0,"state3":0};
+      var outputCount = 0;
+      var last = false;
+
+      function inputValidator(msg) {
+         msg.should.not.have.property('error');
+         msg.should.have.property('state', state);
+         outputCount++;
+         outputCounters[state]++;
+      }
+
+      h1.on("input", inputValidator);
+
+      h2.on("input", inputValidator);
+
+      h3.on("input", inputValidator);
+
+      h4.on("input", inputValidator);
+
+      state = "start";
+      n1.fsm.state.should.equal(state);
+
+      state = "state1";
+      n1.receive({ topic: "t1" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t1" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t3" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t4" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "invalid" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state2";
+      n1.receive({ topic: "t2" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t1" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t2" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t4" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state3";
+      n1.receive({ topic: "t3" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t1" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t2" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "t3" });
+      n1.fsm.state.should.equal(state);
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state1";
+      n1.receive({ topic: "t4" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "start";
+      n1.receive({ topic: "r" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state1";
+      n1.receive({ topic: "t1" });
+      expectedCounts[state]++;
+      state = "state2";
+      n1.receive({ topic: "t2" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      state = "start";
+      n1.receive({ topic: "r" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state1";
+      n1.receive({ topic: "t1" });
+      expectedCounts[state]++;
+      state = "state2";
+      n1.receive({ topic: "t2" });
+      expectedCounts[state]++;
+      state = "state3";
+      n1.receive({ topic: "t3" });
+      expectedCounts[state]++;
+      n1.fsm.state.should.equal(state);
+      state = "start";
+      n1.receive({ topic: "r" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      should.equal(outputCount, 12);
+
+      done();
+    });
+  });
+
+  it("msg directed to output for current state", function (done) {
+    helper.load([stateMachineNode, catchNode], flow4, function () {
+      var n1 = helper.getNode("n1");
+      var h1 = helper.getNode("h1");
+      var h2 = helper.getNode("h2");
+      var h3 = helper.getNode("h3");
+      var h4 = helper.getNode("h4");
+
+      var state;
+      var outputCounters = {"start":0,"state1":0,"state2":0,"state3":0};
+      var expectedCounts = {"start":0,"state1":0,"state2":0,"state3":0};
+      var outputCount = 0;
+      var last = false;
+
+      function inputValidator(msg) {
+         msg.should.not.have.property('error');
+         msg.should.have.property('state', state);
+         outputCount++;
+         outputCounters[state]++;
+      }
+
+      h1.on("input", inputValidator);
+
+      h2.on("input", inputValidator);
+
+      h3.on("input", inputValidator);
+
+      h4.on("input", inputValidator);
+
+      state = "start";
+      n1.fsm.state.should.equal(state);
+
+      state = "state1";
+      n1.receive({ topic: "t1" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "invalid" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state2";
+      n1.receive({ topic: "t2" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "invalid" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state3";
+      n1.receive({ topic: "t3" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      n1.receive({ topic: "invalid" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state1";
+      n1.receive({ topic: "t4" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      state = "state1";
+      n1.receive({ topic: "t1" });
+      expectedCounts[state]++;
+      state = "state2";
+      n1.receive({ topic: "t2" });
+      expectedCounts[state]++;
+      state = "state3";
+      n1.receive({ topic: "t3" });
+      expectedCounts[state]++;
+      n1.fsm.state.should.equal(state);
+      state = "start";
+      n1.receive({ topic: "r" });
+      n1.fsm.state.should.equal(state);
+      expectedCounts[state]++;
+      should.equal(outputCounters[state], expectedCounts[state]);
+
+      should.equal(outputCount, 11);
+
+      done();
+    });
+  });
 
 });
